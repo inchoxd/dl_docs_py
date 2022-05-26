67bb70f0edbae0c3a2bd5f558c3546d225015df5
inchoxd <rjb2066@gmail.com>
2022-05-26 17:57:49 +0900

    SAVED IT ANYWAY!!!!

--- dl_docs/main.py
+++ dl_docs/main.py
@@ -1,8 +1,9 @@
 import requests
 import argparse
+import re
 
 class dl_docs:
-    def dl(self, url, end_page, dl_path=None, start_page=1, z_padding=None):
+    def dl(self, url, end_page=0, dl_path=None, z_padding=False, force=False, trials=0):
         exts = ['.pdf', '.PDF', '.pptx', '.docx', '.png', ',PNG', '.jpg', '.JPG', '.jpeg', '.JPEG']
         if dl_path is None:
             dl_path = './'
@@ -16,18 +17,35 @@ class dl_docs:
         splt_url = url.split('.')
         tmp_name = splt_url[-2]
         dl_name = tmp_name.split('/')[-1]
+        str_num = re.compile(r'\d+').findall(dl_name)[0]
         ext = splt_url[-1]
         if ext not in exts:
             return False
-        dl_path = f'{dl_path}{dl_name}.{ext}'
 
-        r = requests.get(url=url)
-        status_code = r.status_code
-        if status_code != 200:
-            return False
+        if z_padding:
+            fills = int(len(str_num))
+        else:
+            fills = None
+
+        loop_st = True
+        start_page = int(str_num)
+        while(loop_st or start_page <= end_page):
+            if fills:
+                str_num = str_num.zfill(fills)
+                
+            r = requests.get(url=url)
+            status_code = r.status_code
+            if status_code != 200:
+
+                return False
             
-        data = r.content
-        with open(dl_path, 'wb') as f:
-            f.write(data)
+            data = r.content
+            with open(dl_path, 'wb') as f:
+                f.write(data)
 
         return True
+
+
+    def ln_arg(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('url')
