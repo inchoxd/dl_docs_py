0964b8703701ad0a1d73f9bc97d9ab5d093c28c0
inchoxd <rjb2066@gmail.com>
2022-05-26 16:49:03 +0900

    created function of dl

--- dl_docs/main.py
+++ dl_docs/main.py
@@ -1,10 +1,30 @@
 import requests
 import argparse
 
+class dl_docs:
+    def dl(self, url, end_page, dl_path=None, start_page=1, z_padding=None):
+        if dl_path is None:
+            dl_path = './'
+        else:
+            if dl_path[-1] != '/':
+                dl_path = f'{dl_path}/'
 
-def main():
-    pass
+        if 'http' not in url:
+            return False
+            
+        splt_url = url.split('.')
+        tmp_name = splt_url[-2]
+        dl_name = tmp_name.split('/')[-1]
+        ext = splt_url[-1]
+        dl_path = f'{dl_path}{dl_name}.{ext}'
 
+        r = requests.get(url=url)
+        status_code = r.status_code
+        if status_code != 200:
+            return False
+            
+        data = r.content
+        with open(dl_path, 'wb') as f:
+            f.write(data)
 
-if __name__ == '__main__':
-    main()
+        return True
